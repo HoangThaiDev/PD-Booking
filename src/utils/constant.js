0d34c2e86faf3ea7e0358eb1b4e3297af9325a66
HoangThaiDev <thaindev2000@gmail.com>
2024-05-09 22:12:19 +0700

    Fix Domain

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,10 +1,10 @@
 let apiRoot = "";
-
+console.log(import.meta.env);
 if (import.meta.env.DEV === true) {
   apiRoot = "http://localhost:5000";
 }
 if (import.meta.env.PRODUCTION === true) {
   apiRoot = "https://server-pd-booking.onrender.com";
 }
-console.log(apiRoot);
+
 export const API_ROOT = apiRoot;
